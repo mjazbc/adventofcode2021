42045ec3570a2c181d6528e4df6d161deca0e28e
Matic Jazbec <maticj@gen-i.si>
2021-12-11 10:36:22 +0100

    day 7

---
+++ AdventOfCode2021/AdventOfCode2021/Day07.cs
@@ -0,0 +1,48 @@
+﻿using aoc_core;
+
+namespace AdventOfCode2021
+{
+    internal class Day07 : AdventPuzzle
+    {
+        public override string SolveFirstPuzzle()
+        {
+            var positions = Input.AsIntArray(",");
+            var maxPosition = positions.Max();
+            var minFuel = int.MaxValue;
+  
+
+            for(int i = 0; i < maxPosition; i++)
+            {
+                var fuel = positions.Sum(pos => Math.Abs(pos - i));
+                if(fuel < minFuel) 
+                { 
+                    minFuel = fuel;
+
+                }
+            }
+
+            return minFuel.ToString();
+        }
+
+        public override string SolveSecondPuzzle()
+        {
+            var positions = Input.AsIntArray(",");
+            var maxPosition = positions.Max();
+            var minFuel = int.MaxValue;
+
+
+            for (int i = 0; i < maxPosition; i++)
+            {
+                var fuel = positions.Sum(pos => CalculateFuel(Math.Abs(pos - i)));
+                if (fuel < minFuel)
+                {
+                    minFuel = fuel;
+                }
+            }
+
+            return minFuel.ToString();
+        }
+
+        private static int CalculateFuel(int n) => n * (1 + n) / 2;
+    }
+}
